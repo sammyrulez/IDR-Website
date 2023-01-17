f4efb161430d20a796c038fdf36b27d795c24e14
Sam <sam@dev.com>
2023-01-17 18:35:00 +0100

    send data

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 function send_signinForm_data(){
     $.ajax({
         type : 'POST',
-        url : 'url',
+        url : 'https://script.google.com/macros/s/AKfycbxUxHIBkClK-Ko8d_dsz_hFwBoBVOvOiD1jY-Q0kUXPTH3R5DYskS-hl7GmeSTibjyG/exec',
         data : $('#signinForm').serialize() 
     }).done(function() {
         alert( "second success" );
