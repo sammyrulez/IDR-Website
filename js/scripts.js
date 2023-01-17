59bf842a8d4075cdfbdf2d306b3bbf113e8eab6a
Sam <sam@dev.com>
2023-01-17 18:53:19 +0100

    form submission

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,17 @@
-function send_signinForm_data(){
-    $.ajax({
-        type : 'POST',
-        url : 'https://script.google.com/macros/s/AKfycbxUxHIBkClK-Ko8d_dsz_hFwBoBVOvOiD1jY-Q0kUXPTH3R5DYskS-hl7GmeSTibjyG/exec',
-        data : $('#signinForm').serialize() 
-    }).done(function() {
-        alert( "second success" );
-      })
-      .fail(function() {
-        alert( "error" );
-      })
+function send_signinForm_data() {
+  $("#fieldset1").children().attr("disabled", "disabled");
+  $.ajax({
+    type: "POST",
+    url: "https://script.google.com/macros/s/AKfycbxUxHIBkClK-Ko8d_dsz_hFwBoBVOvOiD1jY-Q0kUXPTH3R5DYskS-hl7GmeSTibjyG/exec",
+    data: $("#signinForm").serialize(),
+  })
+    .done(function () {
+      var myModal = new bootstrap.Modal(
+        document.getElementById("exampleModal")
+      );
+      myModal.show();
+    })
+    .fail(function () {
+      alert("error");
+    }); 
 }
-
